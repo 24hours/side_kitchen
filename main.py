5ab535069866738308c3ca50d8c9aa84bd699b2e
Lee Yeong Khang <sindrosa.24@gmail.com>
2015-08-24 06:52:13 +0900

    change jinja variable delimiter to [[ ]] to prevent future clash with angular js

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+class AngularFlask(Flask):
+    jinja_options = Flask.jinja_options.copy()
+    jinja_options.update(dict(
+        block_start_string='{%',
+        block_end_string='%}',
+        variable_start_string='[[',
+        variable_end_string=']]',
+        comment_start_string='##',
+        comment_end_string='#3',
+    ))
+
+app = AngularFlask(__name__)
 
 @app.route("/")
 def hello():
